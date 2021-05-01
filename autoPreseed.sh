395c1bf68aeb0f5e603a953625c864e15781c4c3
Dominik Haßelkuss <Domi-cc@users.noreply.github.com>
2021-05-01 14:33:03 +0200

    some fixes to use autodownloader

--- autoPreseed.sh
+++ autoPreseed.sh
@@ -1,11 +1,14 @@
-#!bin/bash
+#!/bin/bash
 
 # Skript zur automatischen Erstellung der preseeded Iso mit debian 10.2.0
 # Downloads und Verzeichnis anlegen
-wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-10.2.0-amd64-netinst.iso
+
+# //TODO: check if debian.iso exists or run downloadiso.sh
+#wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-10.2.0-amd64-netinst.iso
+
 wget https://raw.githubusercontent.com/Domi-cc/unattended-debian-installation/master/preseed.cfg
 mkdir tempOrdnerPreseed
-bsdtar -C tempOrdnerPreseed -xf debian-10.2.0-amd64-netinst.iso
+bsdtar -C tempOrdnerPreseed -xf debian.iso
 # preseed anhängen
 chmod +w -R tempOrdnerPreseed/install.amd/
 gunzip tempOrdnerPreseed/install.amd/initrd.gz
@@ -21,8 +24,8 @@ cd ..
 # Neue Iso erstellen; hat bei mir nicht ohne root Rechte funktioniert
 sudo genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat \
             -no-emul-boot -boot-load-size 4 -boot-info-table \
-            -o Preseed-10.2.0-debianInstaller.iso tempOrdnerPreseed
+            -o debian-unattended.iso tempOrdnerPreseed
 # Aufräumen
 chmod +w -R tempOrdnerPreseed/
 rm -r tempOrdnerPreseed
-rm preseed.cfg debian-10.2.0-amd64-netinst.iso
+rm preseed.cfg
